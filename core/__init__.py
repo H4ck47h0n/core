c7cb1943e56808c4ccbee118797b3771d8c52f4e
dengqi <dengqi935@outlook.com>
2016-05-21 20:23:00 +0800

    创建了基本的框架，包括登陆的实现和账户的基本api

--- core/__init__.py
+++ core/__init__.py
@@ -8,11 +8,11 @@ import flask.ext.login as flask_login
 
 lm = flask_login.LoginManager()
 basedir = os.path.abspath(os.path.dirname(__file__))
-db_dir  = os.path.join(basedir,db)
+db_dir  = os.path.join(basedir,"db")
 sqlite_dir = "sqlite:////" + db_dir
-app.config["SQLALCHEMY_DATABASE_URL"] = 'sqlite:////'
 app = Flask(__name__)
 db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URL"] = db_dir
 lm.init_app(app)
 import core.models
 import core.views

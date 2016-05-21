38097fd0005baa5599d2357672a4ab3224dc9696
dengqi <dengqi935@outlook.com>
2016-05-21 19:42:42 +0800

    update the datebase

--- core/models.py
+++ core/models.py
@@ -8,8 +8,10 @@
      password  = db.Column(db.String(120))
      like      = db.Column(db.String(1200))
      dislike   = db.Column(db.String(1200))
-     def __init__(self,username,password):
+     exiting   = db.Column(db.String(120))
+     def __init__(self,username,password,exiting):
          self.username  =  username
          self.password  =  password
+         self.exiting   =  exiting
      def __repr__(self):
          return "<ACCOUNT NAME IS %r>"%self.username
--- core/views.py
+++ core/views.py
@@ -2,9 +2,10 @@
 # encoding: utf-8
 
 from . import app,db,lm
+from flask.ext.login import flask_login
 from .models import User
 
-from flask import request, render_template
+from flask import request, render_template, jsonfy
 
 @lm.user_loader
 def user_loader(username):
@@ -18,11 +19,31 @@ def request_loader(request):
 
 @app.route("/login" methods=["GET","POST"])
 def login():
-    if request.methods = "POST":
+    if request.method  == "POST":
         username =  request.get_json()['username']
         password =  request.get_json()['password']
-
-
+        if password = users[username]['pw']:
+            user          = User()
+            user.username = username
+            flask_login.login_user(user)
+            return jsonfy(
+                {"status":"success",
+                 "message":"login success"
+            })
+        else:
+            return jsonfy({
+                "status":"false",
+                "message":"The password is wrong"
+            })
+    if request.method == "GET":
+        return render_template("login.html")
+@app.rout("/logout")
+def logout():
+    flask_login.logout_user()
+    return jsonfy({
+        "status":"success",
+        "message":"logout success"
+    })
 
 
 #app.run(host="0.0.0.0",debug=True)
